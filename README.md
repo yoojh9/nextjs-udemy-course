2ffea31e815be777a4b3c737fd48a6851cf1f794
yoojh9 <yoojh9@gmail.com>
2023-12-11 15:30:28 +0900

    starting code

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# API Route 프로젝트
+# API Route 프로젝트 실습
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-function HomePage() {
+import Head from 'next/head';
+
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
+
+function HomePage(props) {
   return (
     <div>
-      <h1>The Home Page</h1>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
