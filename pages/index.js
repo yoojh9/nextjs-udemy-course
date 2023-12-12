d5c835f05967942783a88fe21729e8a6333ab46a
yoojh9 <yoojh9@gmail.com>
2023-12-12 16:30:46 +0900

    Hero 컴포넌트 추가하기

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ pages/posts/[slug].js
@@ -0,0 +1,3 @@
+const PostDetailpage = () => {};
+
+export default PostDetailpage;
