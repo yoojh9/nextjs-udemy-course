4fb86ab0821cb29890aad5794a9fb87cbbc5e552
yoojh9 <yoojh9@gmail.com>
2023-12-14 15:57:17 +0900

    _app.js 수정 추가

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
